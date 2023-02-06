2ad9858f819d519f4d82c10d37f31dcd6d64ae2e
XPC <XPC@DESKTOP-8B1E7AP>
2023-02-06 14:23:10 -0600

    https://github.com/JuanPablo273/EA-08-Practicas-2.git

---
+++ src/main/java/com/GymWebSite/GymWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.GymWebSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymWebSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymWebSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GymWebSite/GymWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GymWebSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymWebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
